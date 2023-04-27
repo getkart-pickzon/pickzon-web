6200bc199f479bac39c2b32f12333507c8154588
khusyalgetkart <khusyal.singh@getkart.com>
2023-04-27 08:36:57 +0000

    fresh

--- build/static/js/854.95b29088.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkpickzon_web=self.webpackChunkpickzon_web||[]).push([[854],{2854:function(e,n,c){c.r(n);var u=c(4554),r=(c(2791),c(184));n.default=function(){return(0,r.jsx)(u.Z,{p:"1rem",children:"Page"})}}}]);
-//# sourceMappingURL=854.95b29088.chunk.js.map
--- src/pages/dynamicPages/savedPostModule/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SavedPostModule = () => {
-    return (
-        <div>SavedPostModule</div>
-    )
-}
-
-export default SavedPostModule;
